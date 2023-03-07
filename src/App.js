68db13a27246ae44694a223cfc03d62fee668c3f
Sumeyye Yegen <sumeyyeyegen746@gmail.com>
2023-03-07 19:35:59 +0300

    "@chatscope/chat-ui-kit-react" added and message-ui created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {MainContainer,ChatContainer,MessageList,Message,MessageInput,TypingIndicator} from '@chatscope/chat-ui-kit-react';
+import {useState} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{position:"relative",height:"600px", width:"700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+            >
+              {
+                messages.map((item,i) => {
+                  return <Message key={i} model={item}/>
+                })
+              }
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
